7e0913d47f44334ac9869a1e30232fb40bc2a1a3
wonho <wonho951@gmail.com>
2021-07-22 10:52:08 +0900

    2021.07.21 수업내용(spring)

--- src/main/java/com/javaex/controller/PhoneController.java
+++ src/main/java/com/javaex/controller/PhoneController.java
@@ -2,6 +2,7 @@ package com.javaex.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller	//-->얘도 임포트 해줘야함
 public class PhoneController {
@@ -9,6 +10,31 @@ public class PhoneController {
 	//생성자
 	//메소드 g/s
 	//메소드 - 일반
+	@RequestMapping(value="list", method = {RequestMethod.GET, RequestMethod.POST})	//여러개 쓰려면 {}써주기
+	public String list() {
+		System.out.println("[PhoneController]");
+		return "";//jsp 위치 써줌
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	@RequestMapping(value="/test")	//이름 주는거 action역할
 	public String test() {
